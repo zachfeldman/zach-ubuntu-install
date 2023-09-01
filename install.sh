1f6d9673a78f362db8036505a6e28932fe4b45e9
Zach Feldman <zmf@frame.work>
2023-08-31 20:27:55 -0400

    Initial commit

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# : Install Git
+# sudo apt install git -y
+
+# : Install Chrome
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# : Installing pip3 for sudo user
+# sudo su - <<EOF
+# apt install pip
+# EOF
+
+# : Install Kinto
+# /bin/bash -c "$(wget -qO- https://raw.githubusercontent.com/rbreaves/kinto/HEAD/install/linux.sh || curl -fsSL https://raw.githubusercontent.com/rbreaves/kinto/HEAD/install/linux.sh)"
+
+# : Install Spotify
+# sudo snap install spotify
+
+# : Install Tilix
+# sudo apt-get install tilix -y
+
+# : Install 1Password
+# curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+# echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+# sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+# curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+# sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+# curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+# sudo apt update && sudo apt install 1password -y
+
+# : Install Steam
+# sudo add-apt-repository multiverse
+# sudo apt-get update -y
+# sudo apt-get install steam -y
+
+# : Install PulseEffects
+# sudo apt install pulseeffects -y
+
+: Install PCSCD Needed for Yubico Authenticator
+sudo apt install pcscd -y
+
+# : Set 12 hour time format
+# gsettings set org.gnome.desktop.interface clock-format 12h
+
+# : Set dock favorites
+# gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'google-chrome.desktop', 'sublime_text.desktop', 'com.gexperts.Tilix.desktop', '1password.desktop', 'spotify_spotify.desktop', 'com.github.wwmm.pulseeffects.desktop']"
